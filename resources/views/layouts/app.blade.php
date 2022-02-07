c79ecd254398de189ee3e7e62ed2bcdc8b55f230
aymane <aymane.199820.mehdi@gmail.com>
2022-02-07 20:25:37 +0100

    add migration and controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,8 +7,8 @@
     <title>Document</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
-<body class="bg-gray-100">
-    <nav class="p-6 bg-white flex justify-between">
+<body class="bg-gray-300">
+    <nav class="p-6 bg-white flex justify-between mb-6">
         <ul class="flex items-center">
             <li>
                 <a href="" class="p-3">Home</a>
@@ -20,19 +20,12 @@
                 <a href="" class="p-3">Post</a>
             </li>
         </ul>
-
-        <ul class="flex items-center">
-            <li>
-                <a href="" class="p-3">Login</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Register</a>
-            </li>
-            <li>
-                <a href="" class="p-3">Logout</a>
-            </li>
-        </ul>
     </nav>
+    <style>
+        a{
+            padding: 10px;
+        }
+    </style>
     @yield('content')
 </body>
 </html>
