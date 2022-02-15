ab46b5a01b7a178b5bff04b9decf91bba5876583
aymane <aymane.199820.mehdi@gmail.com>
2022-02-15 20:42:50 +0100

    lu

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -10,7 +10,13 @@ class RegisterController extends Controller
     public function index(){
         return view('auth.register');
     }
-    public function store(){
-        dd('abc');
+    public function store(Request $request){
+        $this->validate($request,[
+            'name'=>'required|max:255',
+            'username'=>'required|max:255',
+            'email'=>'required|email|max:255',
+            'password'=>'required|confirmed',
+        ]);
+        dd('store');
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -9,6 +9,7 @@
                   Name
                 </label>
                 <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Name" type="text" placeholder="Name">
+                
             </div>
             <div class="mb-4">
                 <label class="block text-gray-700 text-sm font-bold mb-2" for="Username">
